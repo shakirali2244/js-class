5f62bcdcac82e83d7923aef5e3ce4b105d2c872b
shakirali2244 <shakirali2244@yahoo.com>
2015-04-26 22:37:10 -0700

    adding socket io

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var pg = require('pg');
 var conString = "postgres://ora:pasasword@localhost/oradb";
- 
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 pg.connect(conString, function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-  client.query('CREATE TABLE users(name varchar, email varchar , password varchar);', function (data){
+  client.query('CREATE TABLE IF NOT EXISTS users(name varchar, email varchar , password varchar);', function (data){
   	console.log(data);
   });
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on("toggle",function(data){
+  	console.log(data);
+  })
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
